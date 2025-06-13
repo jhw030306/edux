f26432844a3a5bdc33448fc08b4d2112117e30c2
jhw030306 <jhw030306@naver.com>
2025-06-13 15:30:30 +0900

    examoff css 수정

--- vite.config.js
+++ vite.config.js
@@ -20,11 +20,11 @@ export default defineConfig({
   server: {
     proxy: {
       "/ws": {
-        target: "http://::8080",
+        target: "http://",
         ws: true,
       },
       "/api": {
-        target: "http://:8080",
+        target: "http://",
         changeOrigin: true,
         secure: false,
       },
